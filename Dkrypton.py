66009ffdc8bb6429f7821dd30edcff1093d7b442
Manu <116085483+theperk08@users.noreply.github.com>
2023-04-19 19:08:33 +0200

    Update Dkrypton.py
    Add comment

--- Dkrypton.py
+++ Dkrypton.py
@@ -2,33 +2,27 @@
 
 import sys
 
-from dkrypton.view6 import DkryptonUi
-from dkrypton.controller6 import DkryptonCtrl
-#from dkrypton.model1 import evaluateExpression
-from dkrypton.model6 import Models
+from dkrypton.view import DkryptonUi
+from dkrypton.controller import DkryptonCtrl
+from dkrypton.model import Models
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication
 
-
-
-__version__ = "0.1"
+__version__ = "1.0"
 __author__ = "Manuel Musy"
 
 def main():
     """
     Départ du programme principal
     """
+    
     dkrypt = QApplication(sys.argv)
-    view = DkryptonUi()   # Render calculator GUI
-    view.show()         # Display calculator GUI
-    model = Models          # Create instance of model
-    DkryptonCtrl(model=model, view=view)  # Create instance of controller
-    sys.exit(dkrypt.exec())    # Execute application main loop
+    view = DkryptonUi()   # Appel du GUI
+    view.show()         # Affichage du GUI
+    model = Models          # Creation instance du model
+    DkryptonCtrl(model=model, view=view)  # Creation instance du controller
+    sys.exit(dkrypt.exec())    # Execution de l'application en boucle
     
 if __name__ == "__main__":
     main()
-
-    
-
-    
